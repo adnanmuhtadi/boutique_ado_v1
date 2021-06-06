9402e8fc778d5c55d4b470e78a44c958d7edc641
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-06-06 12:53:05 +0100

    moving to local

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -172,6 +177,10 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+STRIPE_PUBLIC_KEY = env('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
+
+if 'STRIPE_WH_SECRET' in os.environ:
+    STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", '')
+else:
+    STRIPE_WH_SECRET = env("STRIPE_WH_SECRET")
