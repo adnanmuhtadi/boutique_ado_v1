53886e1791465ec6e7cab82eabe919e0a2d7949a
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-06-09 01:43:37 +0100

    email

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -206,11 +206,35 @@ if 'USE_AWS' in os.environ:
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
+if 'STRIPE_PUBLIC_KEY' in os.environ:
+    STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", '')
+else:
+    STRIPE_PUBLIC_KEY = getenv("STRIPE_PUBLIC_KEY")
+
+if 'STRIPE_SECRET_KEY' in os.environ:
+    STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", '')
+else:
+    STRIPE_SECRET_KEY = getenv("STRIPE_SECRET_KEY")
+
 if 'STRIPE_WH_SECRET' in os.environ:
     STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", '')
-else: 
-    STRIPE_WH_SECRET = env("STRIPE_WH_SECRET")
-    
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    STRIPE_WH_SECRET = getenv("STRIPE_WH_SECRET")
+
+if 'EMAIL_HOST_USER' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
